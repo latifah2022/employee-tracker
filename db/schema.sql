976cdb9d9a0e7a1abb7824a7e65a0f0515b1b6ea
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-24 12:25:42 -0700

    8commit

--- db/schema.sql
+++ db/schema.sql
@@ -23,3 +23,5 @@ CREATE TABLE employee(
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
+
+SELECT * FROM departments;
--- index.js
+++ index.js
@@ -23,3 +23,16 @@ const getDepartments = () => {
     }
     )
 };
+
+const getEmployees = () => {
+    db.query("SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, roles.title, roles.salary, roles.department_id, departments.name FROM employee JOIN roles on employee.role_id=roles.id JOIN departments ON roles.department_id=departments.id", (err, data) => {
+        if (err) {
+            console.log(err)
+            db.end();
+        } else {
+            console.table(data);
+            promptUser();
+        }
+    }
+    )
+};
