db178353ab941f93ee01068e34938b63f18d365a
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-24 09:22:25 -0700

    3commit

--- db/schema.sql
+++ db/schema.sql
@@ -14,3 +14,12 @@ CREATE TABLE roles (
     department_id INT NOT NULL,
     FOREIGN KEY(department_id) REFERENCES departments(id)
 );
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
