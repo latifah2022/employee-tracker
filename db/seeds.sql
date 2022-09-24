b24799fa9d353c1d36384583d5dc880631f8d440
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-24 09:47:31 -0700

    5commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,9 +12,16 @@ VALUES
 ('HR Representative', 36000, 3),
 ('Frontend Developer', 35000, 1),
 ('Receptionist', 20000, 3);
+('Backend', 30000, 2);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id) 
 VALUES
-('Erik', 'Lew', 1, null),
-('Mike', 'Lowry', 2, 1),
-('John', 'Snow', 3, 1);
+('Tom', 'Lew', 1, null),
+('Mike', 'Smith', 2, 1),
+('John', 'Snow', 3, 1),
+("Michelle", "Myers", 3, 1),
+("Gill", "Frankel", 5, 1),
+("Sherry", "McDonald", 3, 1),
+("Katie", "Cormack", 4, 1),
+("Kendall", "Mancino", 3, 1),
+("Mark", "Rowley", 3, 1);
